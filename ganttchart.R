f93b9438b3dbbb948478ca656035bf2d8de85c29
amummert2 <31447643+amummert2@users.noreply.github.com>
2017-09-29 15:07:15 -0400

    Update ganttchart.R

--- ganttchart.R
+++ ganttchart.R
@@ -11,5 +11,5 @@ gantt.chart(cbind(myPlan, person=c("Andrew", "Letti", "Amanda")),
   colors=c("red", "green", "blue"), main="My Plan", format="%d-%b-  %y",   
   eventTimes=c("02-Oct-2017", "06-Oct-2017", "09-Oct-2017"),   
   eventNames=c("Test Suite Review", "Combine", "Documentation Review"),   
-  priorityNames = c("Low", "Medium", "High"), sub="H. Seltman") 
+  priorityNames = c("Low", "Medium", "High")) 
 
